a733a45033fb140f3ff09bbad671d2b1a93bbade
fiandev <fiandev1234@gmail.com>
2025-10-27 09:17:08 +0700

    feat(pin): update pin reset logic
    Modify PinSetupPage to accept `isForReset` flag.
    Update SecretNoteListPage to navigate to PinSetupPage for reset.
    Refactor PinService.resetPin to handle decryption and re-encryption of notes.

--- lib/services/pin_service.dart
+++ lib/services/pin_service.dart
@@ -1,9 +1,17 @@
 import 'dart:convert';
 import 'package:crypto/crypto.dart';
+import 'package:my_note/services/note_service.dart';
+import 'package:my_note/utils/crypto_helper.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class PinService {
   static const _pinKey = 'user_pin_hash';
+  final NoteService _noteService;
+  final CryptoHelper _cryptoHelper;
+
+  PinService()
+      : _noteService = NoteService(),
+        _cryptoHelper = CryptoHelper();
 
   String _hashPin(String pin) {
     final bytes = utf8.encode(pin);
@@ -27,8 +35,28 @@ class PinService {
     return stored == _hashPin(pin);
   }
 
-  Future<void> resetPin() async {
+  Future<void> resetPin(String newPin, String oldPin) async {
     final prefs = await SharedPreferences.getInstance();
+    final storedPin = prefs.getString(_pinKey);
+
+    if (storedPin != null) {
+      final notes = await _noteService.loadAllNotes();
+      final decryptedNotes = notes.map((note) {
+        if (note.isSecret) {
+        try {
+          note.content =
+              _cryptoHelper.decrypt(note.content, oldPin, note.id);
+        } catch (_) {
+            note.content = '[Encrypted] Wrong PIN or Corrupted data';
+          }
+        }
+        return note;
+      }).toList();
+
+      await _noteService.saveNotes(decryptedNotes, pin: newPin);
+    }
+
     await prefs.remove(_pinKey);
+    await savePin(newPin);
   }
 }
